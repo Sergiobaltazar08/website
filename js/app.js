9b937779be79e72c474a481bb7546d079e5c934d
Sergiobaltazar08 <94560479+Sergiobaltazar08@users.noreply.github.com>
2022-03-24 18:40:33 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -21,7 +21,7 @@ const seccionesPagina = new fullpage('#fullpage', {
 	// ──────────────────────────────────────────────────
 	//   :::::: Secciones
 	// ──────────────────────────────────────────────────
-		 
+		 sectionsColor : ['#c2c2c2', '#000', '#c2c2c2', '#000', '#c2c2c2'], // Color de fondo de cada seccion.
 		 verticalCentered: true, // Si alineara de forma vertical los contenidos de cada seccion.
 	// ──────────────────────────────────────────────────
 	//   :::::: Animaciones (Callbacks de FullPage.js)
